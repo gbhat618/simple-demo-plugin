2a013c0f35e8e44686bec5876d83f7f31b44eeea
guruprasad <gbhat@cloudbees.com>
2024-07-02 22:46:13 +0530

    initial commit

---
+++ src/main/java/io/jenkins/plugins/sample/MyPlugin.java
@@ -0,0 +1,71 @@
+package io.jenkins.plugins.sample;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.QueryParameter;
+import org.springframework.lang.NonNull;
+
+import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.model.ManagementLink;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+
+@Extension
+public class MyPlugin extends ManagementLink implements Describable<MyPlugin> {
+
+    private String field1;
+
+    @Override
+    public String getIconFileName() {
+        return "";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "MyPlugin";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "myplugin";
+    }
+
+    public Category getCategory() {
+        return Category.CONFIGURATION;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Descriptor<MyPlugin> getDescriptor() {
+        return Jenkins.get().getDescriptorOrDie(getClass());
+    }
+
+    @Extension
+    @Symbol("myPlugin")
+    public static class DescriptorImpl extends Descriptor<MyPlugin> {
+
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "Double validation demo";
+        }
+
+        public static AtomicInteger ctField1 = new AtomicInteger(0);
+
+        public FormValidation doCheckField1(@QueryParameter String value) {
+            ctField1.incrementAndGet();
+            return FormValidation.ok("Total number of field1 validation requests: " + ctField1.get());
+        }
+    }
+}
