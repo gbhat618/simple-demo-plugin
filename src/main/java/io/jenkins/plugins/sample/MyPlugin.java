4e2145452f5da9845508af8ca60f4efe3a759825
guruprasad <gbhat@cloudbees.com>
2024-07-10 15:34:06 +0530

    Update the notes within the code itself for self contained notes

--- src/main/java/io/jenkins/plugins/sample/MyPlugin.java
+++ src/main/java/io/jenkins/plugins/sample/MyPlugin.java
@@ -18,6 +18,8 @@ public class MyPlugin extends ManagementLink implements Describable<MyPlugin> {
 
     private String field1;
 
+    private String field2;
+
     @Override
     public String getIconFileName() {
         return "";
@@ -45,6 +47,15 @@ public class MyPlugin extends ManagementLink implements Describable<MyPlugin> {
         this.field1 = field1;
     }
 
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+
     @SuppressWarnings("unchecked")
     @Override
     public Descriptor<MyPlugin> getDescriptor() {
@@ -62,11 +73,52 @@ public class MyPlugin extends ManagementLink implements Describable<MyPlugin> {
         }
 
         public static AtomicInteger ctField1 = new AtomicInteger(0);
+        public static AtomicInteger ctField2 = new AtomicInteger(0);
 
-        public FormValidation doCheckField1(@QueryParameter("field1") String myRenamedParam) {
+        /*
+        Problem 1:
+        
+        Here naming the method parameter other than `value` causes the hudson-behavior.js to register the event twice,
+        The registration of the checker event happens twice, as below
+            1. At L743 in the else block
+                 else {
+                    e.onchange = checker;
+                  }
+
+            2. At L763 as part of the "checkDependsOn" evaluation.
+                        var c = findNearBy(e, name);
+                        if (c == null) {
+                          if (window.console != null) {
+                            console.warn("Unable to find nearby " + name);
+                          }
+                          if (window.YUI != null) {
+                            YUI.log(
+                              "Unable to find a nearby control of the name " + name,
+                              "warn",
+                            );
+                          }
+                          return;
+                        }
+                        c.addEventListener("change", checker.bind(e));
+
+          Suggested Change:
+          Add a check in L750-L751 that `c === e` which means the checkDependsOn is actually the name of the field
+          itself. In which case also log a WARNING that it is resulting in double validation, and developer should
+          name the field as "value" to avoid that.
+         */
+        public FormValidation doCheckField1(@QueryParameter String field1) {
             ctField1.incrementAndGet();
-            System.out.println(myRenamedParam);
+            System.out.println("doCheckField1 " + field1);
             return FormValidation.ok("Total number of field1 validation requests: " + ctField1.get());
+        }
+
+        /*
+
+         */
+        public FormValidation doCheckField2(@QueryParameter String field2, @QueryParameter String field1) {
+            ctField2.incrementAndGet();
+            System.out.println("doCheckField2 " + field1 + " " + field2);
+            return FormValidation.ok("Total number of field2 validation requests: " + ctField2.get());
         }
     }
 }
